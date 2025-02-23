d671043294d4cd32d8eda8a98d8832340b3a00ce
Hiba-Mohamed <hibamoha987@gmail.com>
2025-02-23 20:06:48 -0330

    task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,20 @@
 public class Task {
+    private String taskDescription;
+    private boolean complete;
+
+    public void setTaskDescription(String newDescription){
+        this.taskDescription = newDescription;
+    }
+
+    public void setComplete(boolean newComplete) {
+        this.complete = newComplete;
+    }
+
+    public String getTaskDescription(){
+        return this.taskDescription;
+    }
+
+    public boolean getCompletionStatus(){
+        return this.complete;
+    }
 }
