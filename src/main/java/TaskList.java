2df620e13e0abd87fcaffba66a9ed46380c9137b
Hiba-Mohamed <hibamoha987@gmail.com>
2025-02-23 20:31:11 -0330

    added needed methods in user class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,10 @@
 public class TaskList {
+
+    public void addTask(Task newTask){}
+    public void markTaskComplete(String description){
+
+    }
+    public void printAllTasks(){
+
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,40 @@ public class User {
     private String userName;
     private TaskList userTaskList;
 
+    public User (String newUserName, TaskList newUserTaskList){
+        this.userName = newUserName;
+        this.userTaskList = newUserTaskList;
+    }
+    public User (String newUserName){
+        this.userName = newUserName;
+        this.userTaskList = new TaskList();
+    }
+
+    public void setUserName (String name){
+        this.userName = name;
+    }
+
+    public void  setUserTaskList (TaskList list){
+        this.userTaskList = list;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public TaskList getUserTaskList() {
+        return userTaskList;
+    }
+
+    public void addTask(Task newTask){
+        this.userTaskList.addTask(newTask);
+    }
+
+    public void markTasKComplete(String description){
+       userTaskList.markTaskComplete(description);
+    }
+
+    public void printAllTasks(){
+        this.userTaskList.printAllTasks();
+    }
 }
